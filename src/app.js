efdac085bc77c0883121625b1729418aba2b4b53
Grijalba03 <arieswolve26@gmail.com>
2022-10-22 15:06:17 +0000

    completed

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generarexcusa();
+};
+const generarexcusa = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["cat", "dog", "mice", "little kid"];
+  let action = ["took", "ate", "threw", "stole"];
+  let possetion = [
+    "my homework",
+    "the pages of my book",
+    "the food",
+    "my phone"
+  ];
+  let when = ["last night", "Friday Night", "yesterday."];
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
